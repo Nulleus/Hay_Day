9f6909ff9a4fe6b4bd2baed2b0cca340157e281e
Dmitry <rabeev2@mail.ru>
2021-03-14 23:50:32 +0300

    14.03.2021

---
+++ Assets/Contents.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data.SqlClient;
+using System.Data;
+
+
+public class Contents : MonoBehaviour
+{
+    public string SQLQuery;
+    //public string ConnectionString;
+    public string[] QueryResult;
+    public GameObject User;
+    //string formatForMySql = dateValue.ToString("yyyy-MM-dd HH:mm:ss");
+
+    public void Start()
+    {
+        User = GameObject.Find("User");
+        AddContents("bakery", "bread", "2021-04-14 22:40:00", "2021-04-14 22:45:00", 1, 11);
+    }
+    public void GetServerDateTime()
+    {
+
+    }
+    public void AddContents(string subject_parent, string subject_child, string time_loading,string time_shipment, int output_quantity, int user_id)
+    {
+        
+        //SQLQuery = "SELECT id_user from Users WHERE login='" + UserName + "' AND pasword='" + UserPassword + "'";
+        SQLQuery = "INSERT contents (subject_parent, subject_child, time_loading, time_shipment, output_quantity, user_id) VALUES ('"+subject_parent+ "','" + subject_child + "','" + time_loading + "','" + time_shipment + "'," + output_quantity + "," + user_id + ")";
+        IDbConnection dbcon;
+        using (dbcon = new SqlConnection(User.GetComponent<UserData>().ConnectionString))
+        {
+            dbcon.Open();//Открытие соединения с базой данных
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                dbcmd.CommandText = SQLQuery;
+            }
+        }
+    }
+}
--- Assets/UserData.cs
+++ Assets/UserData.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Data.SqlClient;
+using System.Data;
 
 public class UserData : MonoBehaviour
 {
@@ -10,6 +12,9 @@ public class UserData : MonoBehaviour
     public string UserName;
     public string UserPassword;
     public string ConnectionString;
+
+    public string SQLQuery;
+    public string[] QueryResult;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +24,28 @@ public class UserData : MonoBehaviour
             "DataBase=" + DataBase + ";" + 
             "USER=" + UserName + ";" + 
             "PASSWORD=" + UserPassword + ";";
+        GetIDUser();
     }
 
     // Update is called once per frame
-    void Update()
+    public void GetIDUser() //Получениие уникального идентификатора пользователя
     {
-        
+        SQLQuery = "SELECT id_user from users WHERE login='Admin' AND pasword='123'";
+        IDbConnection dbcon;
+        using (dbcon = new SqlConnection(ConnectionString))
+        {
+            dbcon.Open();//Открытие соединения с базой данных
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                dbcmd.CommandText = SQLQuery;
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        IDUser = ((int)reader["id_user"]);
+                    }
+                }
+            }
+        }
     }
 }
