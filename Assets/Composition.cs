927736c462b039c40ac6b8ad323f6aa5bc2d59a3
Dmitry <rabeev2@mail.ru>
2021-08-18 22:40:17 +0300

    18.08.2021

--- Assets/Composition.cs
+++ Assets/Composition.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Composition : MonoBehaviour
+using Sirenix.OdinInspector;
+public class Composition : SerializedMonoBehaviour
 {
-    //Скрипт хранит данные о составе (ингредиентах) объектов, то есть из чего они состоят и в каком количестве
+    //Скрипт хранит данные о составе (ингредиентах) объектов, то есть из чего они состоят и в каком количестве\
+    [ShowInInspector]
     public Dictionary<string, int> Ingredient;
-    [SerializeField]
+
+
     //private GOArray[] _array;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,8 @@ public class Composition : MonoBehaviour
         Ingredient.Add("empty", 1);
 
     }
+
+
 
     // Update is called once per frame
     void Update()
