38b284cc3c3a3b03b90314913b8b2ba1e5fc278a
Dmitry <rabeev2@mail.ru>
2021-10-17 22:38:16 +0300

    17.10.2021

--- Assets/Translit.cs
+++ Assets/Translit.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 using UnityEngine;
+using System;
 
 public class Translit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Data;
+    public string NameRUS;
+    public string DescriptionRUS;
+    private void Start()
     {
-        
+        NameRUS = GetNameRUSByNameObject("wheat");
+        DescriptionRUS = GetDescriptionRUSByNameObject("wheat");
     }
-
-    // Update is called once per frame
-    void Update()
+    public string GetNameRUSByNameObject(string subjectName) 
     {
-        
+        Debug.Log("method GetNameRUSByNameObject");
+        Debug.Log(Data.GetComponent<Connections>().ConnectionString);
+        MySqlConnection conn = new MySqlConnection(Data.GetComponent<Connections>().ConnectionString);
+        try
+        {
+            Debug.Log("Connecting to MySQL...");
+            conn.Open();
+            var SQLQuery = "SELECT name_rus FROM translit WHERE subject_name='" + subjectName + "'";
+            MySqlCommand cmd = new MySqlCommand(SQLQuery, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+               string nameRUS = (string)reader["name_rus"];
+                Debug.Log(nameRUS);
+                return nameRUS;
+            }
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        conn.Close();
+        Debug.Log("Done.");
+        return "0";
+    }
+    public string GetDescriptionRUSByNameObject(string subjectName) 
+    {
+        Debug.Log("method GetDescriptionRUSByNameObject");
+        Debug.Log(Data.GetComponent<Connections>().ConnectionString);
+        MySqlConnection conn = new MySqlConnection(Data.GetComponent<Connections>().ConnectionString);
+        try
+        {
+            Debug.Log("Connecting to MySQL...");
+            conn.Open();
+            var SQLQuery = "SELECT description_rus FROM translit WHERE subject_name='" + subjectName + "'";
+            MySqlCommand cmd = new MySqlCommand(SQLQuery, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string descriptionRUS = (string)reader["description_rus"];
+                Debug.Log(descriptionRUS);
+                return descriptionRUS;
+            }
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        conn.Close();
+        Debug.Log("Done.");
+        return "0";
     }
 }
--- Assets/price_for_diamonds_panel_slot_0_info_predmet_name_text.cs
+++ Assets/price_for_diamonds_panel_slot_0_info_predmet_name_text.cs
@@ -9,42 +9,24 @@ public class price_for_diamonds_panel_slot_0_info_predmet_name_text : MonoBehavi
     // Start is called before the first frame update
     void Start()
     {
-        
+        var go = new GameObject();//Создаем пустой объект
+        go.name = "Bакегу";//Присваиваем ему имя
+        go.AddComponent<Rigidbody2D>();
+        go.AddComponent<WorldCreation>();
+        string typeName = "UnityEngine.Rigidbody2D, UnityEngine.Physics2DModule";//
+
+        Type myClassType = Type.GetType(typeName);//nony4aeM тип Rigidbody2D go.AddComponent(myClassType);//flo6aBnflev компонент Rigidbody2D созданному объекту go.GetComponent(myClassType);
+
+        dynamic someComp = go.GetComponents(myClassType);
+
+        if (someComp != null) someComp.simulated = false;
+        Debug.Log("0000");
     }
 
     // Update is called once per frame
     void Update()
     {
         //=============crops======================================================//
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "wheat"){gameObject.GetComponent<Text>().text = "Пшеница";}
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "corn"){gameObject.GetComponent<Text>().text = "Кукуруза";}
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "soybean"){gameObject.GetComponent<Text>().text = "Соя";}
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "sugarcane"){gameObject.GetComponent<Text>().text = "Тростник";}
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "carrot"){gameObject.GetComponent<Text>().text = "Морковь";}
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "indigo") { gameObject.GetComponent<Text>().text = "Индиго"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "pumpkin"){gameObject.GetComponent<Text>().text = "Тыква";}
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "apple") { gameObject.GetComponent<Text>().text = "Яблоко"; }
-
-        //============predmets======================================================//
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "empty") { gameObject.GetComponent<Text>().text = "Пусто"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "egg") { gameObject.GetComponent<Text>().text = "Яйцо"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "bread") { gameObject.GetComponent<Text>().text = "Хлеб"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "chicken_feed") { gameObject.GetComponent<Text>().text = "Куриный корм"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "cow_feed") { gameObject.GetComponent<Text>().text = "Коровий корм"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "milk") { gameObject.GetComponent<Text>().text = "Молоко"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "cream") { gameObject.GetComponent<Text>().text = "Сливки"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "corn_bread") { gameObject.GetComponent<Text>().text = "Кукурузный хлеб"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "brown_sugar") { gameObject.GetComponent<Text>().text = "Коричневый сахар"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "popcorn") { gameObject.GetComponent<Text>().text = "Попкорн"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "butter") { gameObject.GetComponent<Text>().text = "Масло"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "pancake") { gameObject.GetComponent<Text>().text = "Блин"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "pig_feed") { gameObject.GetComponent<Text>().text = "Свиной корм"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "bacon") { gameObject.GetComponent<Text>().text = "Бекон"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "cookie") { gameObject.GetComponent<Text>().text = "Печенье"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "bacon_and_aggs") { gameObject.GetComponent<Text>().text = "Яичница с беконом"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "cheese") { gameObject.GetComponent<Text>().text = "Сыр"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "white_sugar") { gameObject.GetComponent<Text>().text = "Белый сахар"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "carrot_pie") { gameObject.GetComponent<Text>().text = "Морковный пирог"; }
-        if (globals.price_for_diamonds_panel_slot_0_predmet_name == "pumpkin_pie") { gameObject.GetComponent<Text>().text = "Тыквенный пирог"; }
+        //if (globals.price_for_diamonds_panel_slot_0_predmet_name == "wheat"){gameObject.GetComponent<Text>().text = "Пшеница";}
     }
 }
