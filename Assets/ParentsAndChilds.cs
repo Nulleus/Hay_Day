a03ac1575eb2e373181b5f5074594ac308847566
Dmitry <rabeev2@mail.ru>
2022-05-06 00:02:07 +0300

    06.05.2022

--- Assets/ParentsAndChilds.cs
+++ Assets/ParentsAndChilds.cs
@@ -7,6 +7,7 @@ using System;
 using Proyecto26;
 using UnityEditor;
 using Sirenix.OdinInspector;
+using UnityEngine.Networking;
 
 //Класс нужен для производственных зданий, чтобы показывать что они производят
 public class ParentsAndChilds : MonoBehaviour
@@ -91,11 +92,42 @@ public class ParentsAndChilds : MonoBehaviour
     {
 
     }
+    IEnumerator postRequest(string url, string json)
+    {
+        var uwr = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+
+        //Send the request then wait here until it returns
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            var b = new ResponseGetSubjectChildName();
+            b = JsonUtility.FromJson<ResponseGetSubjectChildName>(uwr.downloadHandler.text);
+            Debug.Log(b.subjectChildName);
+        }
+    }
+
     private void OnEnable()
     {
+        var a = new POSTGetSubjectChildName();
+        a.jwt = Data.GetComponent<Users>().GetJWTToken();
+        a.methodName = "GetSubjectChildName";
+        a.subjectParentName = "field";
+        a.number = 2;
+        
+        StartCoroutine(postRequest("http://farmpass.beget.tech/api/parent_and_child_execute_methods.php",a.ToString()));
         //GetCountSubjectsParent("bakery");
         //GetSubjectParentName("wheat");
-        GetSubjectChildName("field",0);
+        //GetSubjectChildName("field",0);
     }
     public void GetCountSubjectsParent(string subjectChildName)
     {
