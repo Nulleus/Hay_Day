4a57519747dfc811feb8085aab37ef1622870aa0
Dmitry <rabeev2@mail.ru>
2022-05-30 17:39:49 +0300

    30.05.2022

--- Assets/Storage.cs
+++ Assets/Storage.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class Storage : MonoBehaviour
 {
     public int Capacity;//Вместимость хранилища
+    public string StorageName; //Имя хранилища
+    [ShowInInspector]
+    //SubjectName and SumCount in class SubjectSum
+    public Dictionary<string, int> ContentStorage = new Dictionary<string, int>();
+    //Имя объекта и ссылка на него
+    [ShowInInspector]
+    public Dictionary<string, GameObject> LinkGameObject = new Dictionary<string, GameObject>();
 
-
+    private void OnEnable()
+    {
+        LinkGameObject.Add("wheat", gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
