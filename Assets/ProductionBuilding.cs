45ede359215bb580d1d27a32b02a40332fe0f0e1
Dmitry <rabeev2@mail.ru>
2022-12-04 01:45:07 +0300

    04.12.2022

--- Assets/ProductionBuilding.cs
+++ Assets/ProductionBuilding.cs
@@ -1,13 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Proyecto26;
+using UnityEditor;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+using System.Threading;
+using Sirenix.OdinInspector;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class ProductionBuilding : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Data;
+    [SerializeField]
+    private string SubjectName;
+
+    [Serializable]
+    public class POSTBuySubjectForDiamond
+    {
+        public string jwt;
+        public string methodName;
+        public string subjectName;
+        public override string ToString()
+        {
+            return UnityEngine.JsonUtility.ToJson(this, true);
+        }
+    }
+    
+    [Serializable]
+    public class ResponseBuySubjectForDiamond
+    {
+        public string code;
+        public string message;
+        public override string ToString()
+        {
+            return UnityEngine.JsonUtility.ToJson(this, true);
+        }
+    }
+    public void BuySubjectForDiamond(string subjectName)
+    {
+        EditorUtility.DisplayDialog("code: ", "123", "Ok");
+        RestClient.Post<ResponseBuySubjectForDiamond>("http://farmpass.beget.tech/api/production_building_execute_methods.php", new POSTBuySubjectForDiamond
+        {
+            jwt = Data.GetComponent<Users>().GetJWTToken(),
+            methodName = "BuySubjectForDiamond",
+            subjectName = subjectName
+
+        }).Then(response => {
+            EditorUtility.DisplayDialog("code: ", response.message, "Ok");
+            EditorUtility.DisplayDialog("message: ", response.code, "Ok");
+        });
+    }
+        // Start is called before the first frame update
     void Start()
     {
         
+    }
+    private void OnEnable()
+    {
+        BuySubjectForDiamond(SubjectName);
     }
 
     // Update is called once per frame
