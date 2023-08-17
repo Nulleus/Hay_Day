00b4cedc16c14d92de9f8ac0667a223278f2cdee
Dmitry <rabeev2@mail.ru>
2023-08-17 23:13:15 +0300

    17.08.2023

--- Assets/PriceSubjects.cs
+++ Assets/PriceSubjects.cs
@@ -1,42 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using MySql.Data;
-using MySql.Data.MySqlClient;
 using System;
+using UnityEngine;
+using Proyecto26;
+using UnityEditor;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+using System.Threading;
+using Sirenix.OdinInspector;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Mono.Data.Sqlite; // 1
+using System.Data; // 1
 
 public class PriceSubjects : MonoBehaviour
 {
     public GameObject Data;
     private void Start()
     {
-        Debug.Log("Debug: GetPriceDiamondsByNameSubject=" + GetPriceDiamondsByNameSubject("wheat").ToString());
     }
-    public int GetPriceDiamondsByNameSubject(string subjectName) //Получаем количество продукта на выходе.
+    //Получаем стоимость предмета в указанной валюте
+    [Button(ButtonSizes.Medium, ButtonStyle.FoldoutButton)]
+    public int GetPrice(string subjectName, string currency)
     {
-        Debug.Log("method GetPriceDiamondsByNameSubject");
-        Debug.Log("connectionString: " + Data.GetComponent<Connections>().ConnectionString);
-        MySqlConnection conn = new MySqlConnection(Data.GetComponent<Connections>().ConnectionString);
-        try
-        {
-            Debug.Log("Connecting to MySQL...");
-            conn.Open();
-            var SQLQuery = "SELECT diamonds_for_max_times FROM price_subjects WHERE subject_name='" + subjectName + "' LIMIT 0,1 ";
-            MySqlCommand cmd = new MySqlCommand(SQLQuery, conn);
-            MySqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+        int priceCoin;
+        int priceDiamond;
+        string dbName = "MyDatabase.sqlite";
+        string dbUri = "URI=file:" + Application.persistentDataPath + "/" + dbName + ".db";  // 4
+        if (currency == "coin")
+		{
+		    //echo "GetPrice currency=coin";
+		    //debug
+            Debug.Log("GetPrice currency=coin");
+
+            string sqlQuery = "SELECT coins_for_one_round FROM price_subjects WHERE subject_name =" + "'" + subjectName + "'" + "LIMIT 0,1";
+            Debug.Log("sqlQuery=" + sqlQuery);
+            using (var connection = new SqliteConnection(dbUri))
             {
-                return (int)reader["diamonds_for_max_times"];
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(sqlQuery, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            priceCoin = Convert.ToInt32(reader.GetValue(0));
+                            return priceCoin;
+                        }
+                    }
+                }
             }
-            reader.Close();
         }
-        catch (Exception ex)
-        {
-            Debug.Log(ex.ToString());
-            return -1;
+        //diamonds_for_one_ingredients
+        if (currency == "diamond")
+		{
+            Debug.Log("GetPrice currency=diamond");
+
+            string sqlQuery = "SELECT diamonds_for_one_ingredients FROM price_subjects WHERE subject_name =" + "'" + subjectName + "'" + "LIMIT 0,1";
+            Debug.Log("sqlQuery=" + sqlQuery);
+            using (var connection = new SqliteConnection(dbUri))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(sqlQuery, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            priceDiamond = Convert.ToInt32(reader.GetValue(0));
+                            return priceDiamond;
+                        }
+                    }
+                }
+            }
         }
-        conn.Close();
-        Debug.Log("Done.");
-        return -2;
+        return 0;
+    }
+    //Получить общую стоимоить объектов по списку (предмет, количество)
+    [Button(ButtonSizes.Medium, ButtonStyle.FoldoutButton)]
+    public int GetAllCost(ref Dictionary<string,int> massive)
+    {
+     //Временная переменная
+     int priceSubjectTemp = 0;
+	 //Валюта
+	 string currency = "diamond";
+     //Общая стоимость
+	 int allPriceSubjects = 0;
+            foreach (var d in massive) {
+            //Debug.Log("massive="+massive);
+            //Узнаем стоимость объекта в указанной валюте
+			priceSubjectTemp = GetPrice(d.Key, currency);
+            Debug.Log("priceSubjectTemp="+priceSubjectTemp);
+            //Прибавляем стоимость, так как этот цикл может состоять из нескольких предметов+умножить на количество необходимых ингредиентов
+			allPriceSubjects = allPriceSubjects + (priceSubjectTemp * d.Value);
+            Debug.Log("allPriceSubjects" + allPriceSubjects);
+            }
+            return allPriceSubjects;
     }
 }
