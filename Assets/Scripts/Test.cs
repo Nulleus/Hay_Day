ce0ee64d2cffd0f755d6f4772ffc43d3e9d8c86f
Dmitry <rabeev2@mail.ru>
2025-09-29 00:18:57 +0300

    29.09.2025

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,29 +8,46 @@ using UnityEngine.EventSystems;
 
 public class Test : MonoBehaviour
 {
-    public GameObject TargetPoint;
-
     // Add an impulse which produces a change in angular velocity (specified in degrees).
-    [Button(ButtonSizes.Medium, ButtonStyle.FoldoutButton)]
-    public void AddTorqueImpulse(float angularChangeInDegrees)
+    [System.Serializable]
+    public class LootItem
     {
-        //var body = GetComponent<Rigidbody2D>();
-        //var impulse = (angularChangeInDegrees * Mathf.Deg2Rad) * body.inertia;
-        //body.MoveRotation(100);
-        //body.AddTorque(impulse, ForceMode2D.Impulse);
+        public string Name;
+        [Range(0f, 1f)] public float Probability; // вероятность 0–1
     }
+
+    // Таблица лута можно задать прямо в инспекторе
+    public List<LootItem> lootTable = new List<LootItem>
+    {
+        new LootItem { Name = "Ширпотреб", Probability = 0.76f },
+        new LootItem { Name = "Промышленное качество", Probability = 0.18f },
+        new LootItem { Name = "Засекреченное", Probability = 0.0020f },
+        new LootItem { Name = "Тайное", Probability = 0.0005f }
+    };
+    [Button(ButtonSizes.Medium, ButtonStyle.FoldoutButton)]
     private void Start()
     {
+        // Расчет выпавшего предмета при старте сцены
+        LootItem droppedItem = GetDroppedItem(lootTable);
 
+        if (droppedItem != null)
+            Debug.Log($"Выпал предмет: {droppedItem.Name}");
+        else
+            Debug.Log("Ничего не выпало.");
     }
 
-    private void Update()
+    public LootItem GetDroppedItem(List<LootItem> table)
     {
-        
-    }
-    private void FixedUpdate()
-    {
-        AddTorqueImpulse(100);
+        float randomValue = Random.value; // 0–1
+        float cumulative = 0f;
+
+        foreach (var item in table)
+        {
+            cumulative += item.Probability;
+            if (randomValue <= cumulative)
+                return item;
+        }
+        return null; // если сумма вероятностей < 1
     }
 
     private void OnEnable()
